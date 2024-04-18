91a16a994804350ed597a2cc3417e76ce866587a
Naufal Zufar S <naufal.zufar.s@gmail.com>
2024-04-18 12:30:01 +0700

    feat(server): add event template

--- server/src/services/compile-to-html.js
+++ server/src/services/compile-to-html.js
@@ -3,7 +3,7 @@ import fs from "fs-extra";
 import hbs from "handlebars";
 
 async function compileToHtml(data) {
-  const filepath = path.join(process.cwd(), "src", "templates", "pdf.hbs");
+  const filepath = path.join(process.cwd(), "src", "templates", "event.hbs");
   const templateFile = await fs.readFile(filepath, "utf-8");
   return hbs.compile(templateFile)(data);
 }
